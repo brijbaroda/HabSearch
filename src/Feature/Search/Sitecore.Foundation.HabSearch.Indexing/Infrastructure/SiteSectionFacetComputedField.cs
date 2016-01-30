b3eba79a8a91d4e6bbb610c6212450fc76264e33
bhaveshmaniya <bhaveshmaniya@gmail.com>
2016-01-30 17:12:30 +0530

    Added Computed Field for Faceting & Rename Field

---
+++ src/Feature/Search/Sitecore.Foundation.HabSearch.Indexing/Infrastructure/SiteSectionFacetComputedField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Foundation.HabSearch.Indexing.Infrastructure
+{
+    public class SiteSectionFacetComputedField : IComputedIndexField
+    {
+        public string FieldName { get; set; }
+
+        public string ReturnType { get; set; }
+
+        public object ComputeFieldValue(IIndexable indexable)
+        {
+            var item = (SitecoreIndexableItem)indexable;
+
+            if (item != null && item.Item != null && item.Item.Fields[Templates.FacetType.Fields.SiteSectionFacet] != null)
+            {
+                return item.Item.Fields[Templates.FacetType.Fields.SiteSectionFacet];
+            }
+
+            return string.Empty;
+        }
+    }
+}
--- src/Feature/Search/Sitecore.Foundation.HabSearch.Indexing/Templates.cs
+++ src/Feature/Search/Sitecore.Foundation.HabSearch.Indexing/Templates.cs
@@ -3,7 +3,7 @@ namespace Sitecore.Foundation.HabSearch.Indexing
 {
     using Sitecore.Data;
 
-    internal struct Templates
+    public class Templates
     {
         internal struct IndexedItem
         {
@@ -13,6 +13,17 @@ namespace Sitecore.Foundation.HabSearch.Indexing
             {
                 public static readonly ID IncludeInSearchResults = new ID("{8D5C486E-A0E3-4DBE-9A4A-CDFF93594BDA}");
                 public const string IncludeInSearchResults_FieldName = "IncludeInSearchResults";
+            }
+        }
+
+        internal struct FacetType
+        {
+            public static ID ID = new ID("{388C441F-98B6-4911-AB40-649440970B6B}");
+
+            public struct Fields
+            {
+                public static readonly ID SiteSectionFacet = new ID("{F77A9410-3E65-4E9B-9CD9-7A9CBC4FA81D}");
+                public const string SiteSectionFacet_FieldName = "SiteSectionFacet";
             }
         }
     }
